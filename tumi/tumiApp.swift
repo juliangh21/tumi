bdc054d046e3c88bf1e1c1385524075e4b76249d
juliangh21 <garciahauglandje21@student.cbhs.school.nz>
2026-03-07 17:30:11 -0500

    Initial Commit

---
+++ tumi/tumiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tumiApp.swift
+//  tumi
+//
+//  Created by Julian Garcia-Haugland on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct tumiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
