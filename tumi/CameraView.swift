38b240deeb9336313893a5989a068b990e92526d
JulianGH <158517921+juliangh21@users.noreply.github.com>
2026-06-16 10:24:14 -0400

    Add custom error handling for camera view

--- tumi/CameraView.swift
+++ tumi/CameraView.swift
@@ -8,16 +8,52 @@
 import MijickCamera
 import SwiftUI
 
-
 struct theCameraView: View {
     var theImage: (UIImage?) -> Void
     var body: some View {
         MCamera()
+            .setErrorScreen(CustomCameraErrorScreen.init)
+            .setCameraOutputType(.photo)
             .onImageCaptured{
                 image, controller in
                 controller.closeMCamera()
                 theImage(image)
             }
             .startSession()
+    }
+}
+
+struct CustomCameraErrorScreen: MCameraErrorScreen{
+    let error: MCameraError
+    let closeMCameraAction: () -> ()
+    
+    var body: some View{
+        ZStack{
+            Color.lightbrownbkgrnd
+                .ignoresSafeArea()
+            opensettings(error: error)
+        }
+        
+        
+    }
+}
+
+
+struct opensettings: View {
+    var error: MCameraError
+    @Environment(\.openURL)  var openSettings
+    var body: some View {
+            ZStack(alignment: .center){
+                brownRectangle(width: .infinity, height: CGFloat(45))
+                    .padding(.horizontal)
+                Button(action:{
+                    if let url = URL(string: UIApplication.openSettingsURLString){
+                        openSettings(url)
+                    }
+                }){
+                    Text("Your microphone and/or camera need to be enabled. Open Settings App to change")
+                        .foregroundStyle(Color.mutedgray)
+                }
+            }
     }
 }
